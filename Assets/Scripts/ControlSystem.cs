9d7d0c437a8c53cf017f47fa46ed4408bdf1697d
OWObxxxx <119144405+OWObxxxx@users.noreply.github.com>
2023-03-11 12:01:42 +0800

    20230311_角色操作

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ARCardGame
+{
+    public class ControlSystem : MonoBehaviour
+    {
+        [SerializeField, Header("目標物件")]
+        private Transform target;
+
+        private void Update()
+        {
+            LookAtTarget();
+        }
+
+        /// <summary>
+        /// 面向目標
+        /// </summary>
+        private void LookAtTarget()
+        {
+            transform.LookAt(target);
+
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+    }
+}
+
